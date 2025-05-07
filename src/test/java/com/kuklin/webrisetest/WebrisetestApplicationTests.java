26cf45a417277940022975db40f83b337c3b6f56
DanielK <saponembigas@gmail.com>
2025-05-08 00:39:38 +0700

    init

---
+++ src/test/java/com/kuklin/webrisetest/WebrisetestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuklin.webrisetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebrisetestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
